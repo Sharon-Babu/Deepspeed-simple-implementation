06ca4e42b3376987e267e64cd8915da8d62584f4
Sharon Babu <52823421+Sharon-Babu@users.noreply.github.com>
2021-08-05 10:35:07 +0530

    Update deepspeed.py

--- deepspeed.py
+++ deepspeed.py
@@ -7,28 +7,34 @@ from torch.utils.data import DataLoader, Dataset
 SAMPLE_DIM = 21000
 
 class CustomDataset(Dataset):
+    # defining the dataset
     def __init__(self, samples=42):
         self.dataset = rand(samples, SAMPLE_DIM).cpu().float() * 2 - 1
 
     def __getitem__(self, index):
         return (self.dataset[index], (tsum(self.dataset[index]) > 0).cpu().float())
-
+# returns the length of how many data points are in the dataset
     def __len__(self):
         return self.dataset.size()[0]
-
+    
+# inheritting from lightning module
 class OurModel(LightningModule):
     def __init__(self):
         super(OurModel, self).__init__()
         # Network layers
         self.linear = nn.Linear(SAMPLE_DIM, 2048)
         self.linear2 = nn.Linear(2048, 1)
+        # to predict the probability as an output since probability of anything ranges from 0-1, we use sigmoid function 
         self.output = nn.Sigmoid()
         # Hyper-parameters, that we will auto-tune using lightning!
         self.lr = 0.000001
         self.batch_size = 512
-
+        
+# forward function is used to take a data, transforming it through layers and to get a response from it
     def forward(self, x):
+        # throwing x to the first linear layer and x goes out
         x = self.linear(x)
+        #then throws x to the second linear layer and then x goes out
         x = self.linear2(x)
         output = self.output(x)
         return reshape(output, (-1,))
@@ -36,10 +42,12 @@ class OurModel(LightningModule):
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.lr)
 
+    # train_dataloader() is used to train the data model, in this case our custom dataset
     def train_dataloader(self):
         loader = DataLoader(CustomDataset(samples=43210), batch_size=self.batch_size, shuffle=True)
         return loader
 
+    # function to calculate the training loss values for each samples using binary_cross_entropy()
     def training_step(self, batch, batch_nb):
         x, y = batch
         loss = F.binary_cross_entropy(self(x), y)
@@ -54,6 +62,7 @@ class OurModel(LightningModule):
         loss = F.binary_cross_entropy(self(x), y)
         return {'val_loss': loss, 'log': {'val_loss': loss}}
 
+    # function to calculate the average validation loss
     def validation_epoch_end(self, outputs):
         val_loss_mean = sum([o['val_loss'] for o in outputs]) / len(outputs)
         # show val_acc in progress bar but only log val_loss
